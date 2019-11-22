54c069896d41f1fd508a2f97df8ba3c88b01b931
Aleksey <merzlyakov.aleksey@corezoid.com>
2019-11-22 16:26:57 +0200

    new comm

---
+++ routes/home.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+type ViewData struct{
+	Available bool
+}
+
+
+
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../template/index.html"))
+
+	fmt.Println("Alex \n")
+	posts := ViewData{
+		Available: true,
+	}
+
+	tmpl.Execute(w, posts)
+}
